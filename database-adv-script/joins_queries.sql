5e3fc5a3400be4f68552362d33a7b5134c724db5
sakwasilas <sakwahsilas@gmail.com>
2024-11-28 15:47:43 +0300

    inner join booking table

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -11,3 +11,21 @@ FROM
     Booking b
 INNER JOIN 
     User u ON b.user_id = u.user_id;
+
+SELECT
+    py.payment_id,
+    CONCAT(u.first_name, ' ', u.last_name) AS user_name,
+    p.name AS property_name,
+    py.amount,
+    py.payment_date,
+    py.payment_method
+FROM
+    Payment py
+INNER JOIN
+    Booking b ON py.booking_id = b.booking_id
+INNER JOIN
+    User u ON b.user_id = u.user_id
+INNER JOIN
+    Property p ON b.property_id = p.property_id
+WHERE
+    b.status = 'confirmed';
