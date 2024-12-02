3ce83572fe5f06f8207063a1070b1d870c7b40bf
sakwasilas <sakwahsilas@gmail.com>
2024-12-02 11:21:49 +0300

    added a subquery

--- subqueries.sql
+++ subqueries.sql
@@ -6,3 +6,11 @@ WHERE (
     FROM ratings r
     WHERE r.property_id = p.property_id
 ) > 4.0;
+
+SELECT u.user_id, u.username, u.email
+FROM users u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.user_id
+) > 3;
